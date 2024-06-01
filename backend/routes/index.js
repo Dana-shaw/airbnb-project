3dcac49b5d4a94c42c304800659e523a826b7f87
Dana <dana.shaw95@gmail.com>
2024-06-01 13:05:31 -0500

    Setting up application

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,13 +3,13 @@ const express = require("express");
 const router = express.Router();
 const apiRouter = require("./api");
 
-router.get("/api/csrf/restore", function (req, res) {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    "XSRF-Token": csrfToken,
-  });
-});
+// router.get("/api/csrf/restore", function (req, res) {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     "XSRF-Token": csrfToken,
+//   });
+// });
 
 router.use("/api", apiRouter);
 
@@ -40,8 +40,11 @@ if (process.env.NODE_ENV === "production") {
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== "production") {
   router.get("/api/csrf/restore", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.json({});
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
   });
 }
 
