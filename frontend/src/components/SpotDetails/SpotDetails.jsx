248809e08209a79897abcbeed2cc32fa1abdeb0a
Dana <dana.shaw95@gmail.com>
2024-06-05 19:57:16 -0500

    Directionally correct styling on Spot Details page

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -1,30 +1,40 @@
 import "./SpotDetails.css";
 
 const SpotDetails = ({ spot }) => {
-  console.log(spot)
+  console.log(spot);
   return (
     <div>
-      <h2 className='spot-title'>{spot.name}</h2>
-      <p className='spot-location'>
+      <h2 className="spot-title">{spot.name}</h2>
+      <p className="spot-location">
         {spot.city}, {spot.state}, {spot.country}
       </p>
-      <div className='spot-images'>
+      <div className="spot-images">
         {spot.SpotImages.map((image) => (
-          <img key={image.id} src={image.url} />
+          <img
+            key={image.id}
+            src={image.url}
+            className={`preview-${image.preview}`}
+          />
         ))}
       </div>
-      <div>
-        <h2 className='spot-host'>
-          Hosted by {spot.Owner.firstName} {spot.Owner.lastName}
-        </h2>
-        <p className='spot-description'>{spot.description}</p>
-        <div className='spot-callout'>
-          <p className='callout-price'>${spot.price} night</p>
-          <p className='callout-rating'>{spot.avgStarRating}</p>
-          <button>Reserve</button>
+      <div className="details-container">
+        <div className="details-text">
+          <h2 className="spot-host">
+            Hosted by {spot.Owner.firstName} {spot.Owner.lastName}
+          </h2>
+          <p className="spot-description">{spot.description}</p>
         </div>
-        <h3 className='spot-rating'>{spot.avgStarRating}</h3>
+        <div className="callout-container">
+          <div className="callout-text">
+            <p className="callout-price">${spot.price} night</p>
+            <p className="callout-rating">{spot.avgStarRating}</p>
+          </div>
+          <div className="button-container">
+          <button>Reserve</button>
+          </div>
+        </div>
       </div>
+      <h3 className="spot-rating">{spot.avgStarRating}</h3>
     </div>
   );
 };
